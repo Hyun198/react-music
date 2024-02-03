f65d4133ea16946875074b2ed716b0d2f9fed6d6
Hyun <hwn123h@naver.com>
2024-02-03 16:46:47 +0900

    finish

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,106 @@
-import React, { useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './Card.css'
 import musics from '../data/index'
+import { timer } from '../utils/timer'
 
 function Card({ props: { musicNumber, setMusicNumber, setOpen } }) {
     const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+    const [play, setPlay] = useState(false);
+    const [volume, setVolume] = useState(false);
+    const [showVolume, setShowVolume] = useState(50);
+    const [repeat, setRepeat] = useState('repeat');
 
-    function handleLoadStart() {
-        console.log('load Start');
+    const audioRef = useRef();
+
+
+    function handleLoadStart(e) {
+        const src = e.nativeEvent.srcElement.src;
+        const audio = new Audio(src);
+        audio.onloadedmetadata = function () {
+            if (audio.readyState > 0) {
+                setDuration(audio.duration);
+            }
+        }
+
+        if (play) {
+            audioRef.current.play();
+        }
+    }
+
+    function handlePlayingAudio() {
+        if (play) {
+            audioRef.current.pause();
+            setPlay(false);
+        } else {
+            audioRef.current.play();
+            setPlay(true);
+        }
+    }
+
+    function handleTimeUpdate() {
+        const currentTime = audioRef.current.currentTime;
+        setCurrentTime(currentTime);
+    }
+
+    function changeCurrentTime(e) {
+        const currentTime = Number(e.target.value)
+        audioRef.current.currentTime = currentTime;
+        setCurrentTime(currentTime);
+    }
+
+
+    function handleNextPrev(n) {
+        setMusicNumber(value => {
+            if (n > 0) {
+                return value + n > musics.length - 1 ? 0 : value + n;
+            } else {
+                return value + n < 0 ? musics.length - 1 : value + n;
+            }
+        })
+    }
+
+    useEffect(() => {
+        audioRef.current.volume = volume / 100;
+    }, [volume])
+
+    function handleRepeat() {
+        setRepeat(value => {
+            switch (value) {
+                case 'repeat':
+                    return 'repeat_one'
+                case 'repeat_one':
+                    return 'shuffle'
+                default:
+                    return 'repeat'
+
+            }
+        })
+    }
+
+    function EndedAudio() {
+        switch (repeat) {
+            case 'repeat_one':
+                return audioRef.current.play();
+
+            case 'shuffle':
+                return handleShuffle();
+            default:
+                return handleNextPrev(1);
+        }
+    }
+
+    function handleShuffle() {
+        const num = randomNumber();
+        setMusicNumber(num)
+    }
+
+    function randomNumber() {
+        const number = Math.floor(Math.random() * (musics.length - 1));
+        if (number === musicNumber)
+            return randomNumber();
+
+        return number;
     }
 
     return (
@@ -23,7 +117,7 @@ function Card({ props: { musicNumber, setMusicNumber, setOpen } }) {
             </div>
 
             <div className="img">
-                <img src={musics[musicNumber].thumbnail} alt="" />
+                <img src={musics[musicNumber].thumbnail} alt="" className={`${play ? 'playing' : ''}`} />
             </div>
 
             <div className="details">
@@ -32,49 +126,60 @@ function Card({ props: { musicNumber, setMusicNumber, setOpen } }) {
             </div>
 
             <div className="progress">
-                <input type="range" min={0} max={100} />
+                <input type="range" min={0} max={duration}
+                    value={currentTime} onChange={e => changeCurrentTime(e)}
+                    style={{
+                        background: `linear-gradient(to right, #3264fe ${currentTime / duration * 100} %,
+                        #e5e5e5 ${currentTime / duration * 100} %)`
+                    }} />
             </div>
 
             <div className="timer">
-                <span>00:00</span>
-                <span>03:43</span>
+                <span>{timer(currentTime)}</span>
+                <span>{timer(duration)}</span>
             </div>
 
             <div className="controls">
-                <i className="material-symbols-outlined">
-                    repeat
+                <i className="material-symbols-outlined" onClick={handleRepeat}>
+                    {repeat}
                 </i>
-                <i className="material-symbols-outlined" id="prev">
+                <i className="material-symbols-outlined" id="prev" onClick={() => handleNextPrev(-1)}>
                     skip_previous
                 </i>
-                <div className="play">
+                <div className="play" onClick={handlePlayingAudio}>
                     <i className="material-symbols-outlined" >
-                        play_arrow
+                        {play ? 'pause' : ' play_arrow'}
                     </i>
                 </div>
 
-                <i className="material-symbols-outlined" id="next">
+                <i className="material-symbols-outlined" id="next" onClick={() => handleNextPrev(1)}>
                     skip_next
                 </i>
 
                 {/*volume*/}
-                <i className="material-symbols-outlined"   >
+                <i className="material-symbols-outlined" onClick={() => setShowVolume(prev => !prev)}  >
                     volume_up
                 </i>
 
-                <div className="volume">
-                    <i className="material-symbols-outlined"   >
-                        volume_up
+                <div className={`volume ${showVolume ? 'show ' : ''}`}>
+                    <i className="material-symbols-outlined" onClick={() => setVolume(v => v > 0 ? 0 : 100)}   >
+                        {volume === 0 ? 'volume_off' : 'volume_up'}
                     </i>
-                    <input type="range" min={0} max={100}></input>
-                    <span>50</span>
+                    <input type="range" min={0} max={100} value={volume}
+                        onChange={e => setVolume(Number(e.target.value))}
+                        style={{
+                            background: `linear-gradient(to right, #3264fe ${volume} %,
+                        #e5e5e5 ${volume} %)`
+                        }} />
+                    <span>{volume}</span>
                 </div>
 
 
             </div>
 
-            <audio src={musics[musicNumber].src} hidden
-                onLoadStart={handleLoadStart}
+            <audio src={musics[musicNumber].src} hidden ref={audioRef}
+                onLoadStart={handleLoadStart} onTimeUpdate={handleTimeUpdate}
+                onEnded={EndedAudio}
             />
         </div>
     )
